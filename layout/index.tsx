80a80eed447b7a1961b0c945fdb63cb6c24f053e
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-11-24 23:18:47 +0700

    feat: layout bottom player music

--- layout/index.tsx
+++ layout/index.tsx
@@ -4,28 +4,33 @@ import './layout.css'
 import HeaderLayout from "@/app/violet_spotify_music_components_submodule/layout/header-layout";
 import SidebarLeft from "@/app/violet_spotify_music_components_submodule/layout/sidebar-left";
 import SidebarRight from "@/app/violet_spotify_music_components_submodule/layout/sidebar-right";
+import PlayerBottom from "./player-bottom";
 
 function Layout({ children }: {
   children: React.ReactNode
 }) {
   return (
-    <section className="layout">
-      <div className="header sticky top-0 backdrop-blur rounded-lg mt-[20px] z-50">
+    <>
+      <section className="layout">
+      <div className="header sticky top-0 backdrop-blur rounded-lg pt-[20px] z-50">
         <HeaderLayout />
       </div>
 
-      <div className="leftSide">
-        <SidebarLeft />
+        <div className="leftSide">
+          <SidebarLeft />
+        </div>
+
+        <div className="body">{children}</div>
+
+        <div className="rightSide">
+          <SidebarRight />
+        </div>
+
+      </section>
+      <div className="footer fixed bottom-0 left-14 right-10">
+        <PlayerBottom />
       </div>
-
-      <div className="body">{children}</div>
-
-      <div className="rightSide">
-        <SidebarRight />
-      </div>
-
-      <div className="footer">5</div>
-    </section>
+    </>
   );
 }
 
